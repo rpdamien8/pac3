a587d4e9aaedb12bf74183143ddf4e3e46cc7ed6
rpdamien8 <rpdamien8@gmail.com>
2026-06-21 14:30:02 +0100

    Delete glorp directory

--- glorp/lua/autorun/glorp_pm.lua
+++
@@ -1,3 +0,0 @@
-player_manager.AddValidModel( "Glorp", "models/glorp/glorp_pm.mdl" )
-list.Set( "PlayerOptionsModel", "Glorp", "models/glorp/glorp_pm.mdl" )
-player_manager.AddValidHands( "Glorp", "models/weapons/glorp_carms.mdl", 0, "00000000" )
