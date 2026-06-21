9a01b1dc7158f5ddd8ae38668ef344912f09385a
rpdamien8 <rpdamien8@gmail.com>
2026-06-21 14:28:43 +0100

    Add files via upload

---
+++ glorp/lua/autorun/glorp_pm.lua
@@ -0,0 +1,3 @@
+player_manager.AddValidModel( "Glorp", "models/glorp/glorp_pm.mdl" )
+list.Set( "PlayerOptionsModel", "Glorp", "models/glorp/glorp_pm.mdl" )
+player_manager.AddValidHands( "Glorp", "models/weapons/glorp_carms.mdl", 0, "00000000" )
